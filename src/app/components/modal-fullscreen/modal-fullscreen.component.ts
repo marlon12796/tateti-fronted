e13d8f5d81cc01d6b9c934ed6b4d016ffc1078e6
marlon ureta <fabrisio021@gmail.com>
2024-08-17 23:30:12 -0500

    added confetti winner player

--- src/app/components/modal-fullscreen/modal-fullscreen.component.ts
+++ src/app/components/modal-fullscreen/modal-fullscreen.component.ts
@@ -2,7 +2,7 @@ import { GameState } from '@/app/interfaces/game'
 import { RoomService } from '@/app/services/room.service'
 import { UserService } from '@/app/services/user.service'
 import { animate, animateChild, query, style, transition, trigger } from '@angular/animations'
-import { Component, computed, inject, Input } from '@angular/core'
+import { Component, computed, inject, Input, effect } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { ButtonComponent } from '../button/button.component'
 
@@ -15,8 +15,14 @@ import { ButtonComponent } from '../button/button.component'
   animations: [
     trigger('animateChildren', [transition('* => void', [query('@*', [animateChild()])])]),
     trigger('slideInFromRight', [
-      transition(':enter', [style({ translate: '400px', opacity: 0 }), animate('0.5s ease-in-out', style({ translate: '0', opacity: 1 }))]),
-      transition(':leave', [style({ translate: '0', opacity: 1 }), animate('0.5s ease-in-out', style({ translate: '-400px', opacity: 0 }))])
+      transition(':enter', [
+        style({ translate: '400px', opacity: 0 }),
+        animate('0.5s ease-in-out', style({ translate: '0', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ translate: '0', opacity: 1 }),
+        animate('0.5s ease-in-out', style({ translate: '-400px', opacity: 0 }))
+      ])
     ]),
     trigger('slideInFromRight1', [
       transition(':enter', [style({ translate: '400px' }), animate('0.5s 0.1s ease-in-out', style({ translate: 0 }))]),
@@ -56,6 +62,26 @@ export class ModalFullscreenComponent {
     const validationFinalVictory = state === GameState.FINAL_VICTORY_PLAYER1 || state === GameState.FINAL_VICTORY_PLAYER2
     return validationVictory ? 'Próxima Ronda' : validationFinalVictory ? 'Nueva Partida' : null
   })
+  constructor() {
+    effect(async (onCleanup) => {
+      const state = this.roomService.stateGame()
+      const validationFinalVictory = state === GameState.FINAL_VICTORY_PLAYER1 || state === GameState.FINAL_VICTORY_PLAYER2
+      if (validationFinalVictory) {
+        const confetti = (await import('canvas-confetti')).default
+        const timer = setTimeout(() => {
+          confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+          })
+        }, 1000)
+        onCleanup(() => {
+          clearTimeout(timer)
+        })
+      }
+    })
+  }
+
   newTurn() {
     this.roomService.requestNewTurn()
   }
